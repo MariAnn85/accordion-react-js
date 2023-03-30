7702b937f5da03854f9914cfa7b8279ca42a5f72
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-03-30 10:46:53 +0400

    Update backendData.js

---
+++ backendData.js
@@ -0,0 +1,10 @@
+var backendData = [ { 
+"title":"Section 1", 
+"content":"Our content for the section 1" }, 
+{ 
+"title":"Section 2", 
+"content":"Our content for the section 2" }, 
+{ 
+"title":"Section 3", 
+"content":"Our content for the section 3" 
+} ]
